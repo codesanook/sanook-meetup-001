19fe0688d5ab47417545813f38f4a2589c869ed9
kidchenko <kidchenko@gmail.com>
2019-04-23 21:24:28 +0700

    Finished

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`
+    Hi ${name}
+    Email: ${email}
+    Message: ${message}
+    `)
+    setName("");
+    setEmail("")
+    setMessage("")
   }
+
+  return (
+    <div className="container">
+      <div>
+        <h3>Hi there!</h3>
+        <form onSubmit={submitForm}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            name="name"
+            onChange={e => setName(e.target.value)}
+            className="form-field"
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            className="form-field"
+          />
+          <textarea
+            rows={4}
+            placeholder="Message"
+            value={message}
+            name="message"
+            onChange={e => setMessage(e.target.value)}
+            className="form-field"
+          >
+          </textarea>
+          <button type="submit" className="btn">Send message</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App;
